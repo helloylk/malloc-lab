1ea2fcd8ae46d0801601d692caaa6e9bcb9bec36
helloylk <cessliskal@gmail.com>
2015-11-03 23:08:05 +0900

    Update mm.c

--- mm.c
+++ mm.c
@@ -158,7 +158,7 @@ void* mm_malloc(size_t size)
 {
   size_t asize;       /* Adjusted block size */
   size_t extendsize;  /* Extend heap with this size if no fit free block */
-  char *ptr= NULL;
+  char *ptr;
 
   /* Ignore spurious requests */
   if (size == 0)
